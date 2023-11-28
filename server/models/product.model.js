0dcb2feac0a1762af8e061c938f7bcf97c85f5d2
Fatima-04 <146798356+Fatima-04@users.noreply.github.com>
2023-11-28 13:42:35 -0500

    removed the flavor restrictions

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -7,37 +7,40 @@ Timothy Li – 301201910
 Code Confectioners E-Commerce Website for Bakery
 */
 
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const ProductSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Name is required'],
-        minlength: [3, 'Name must be at least 3 characters long'],
-        maxlength: [50, 'Name must be at most 50 characters long']
-      },
-      price: {
-        type: Number,
-        required: [true, 'Price is required'],
-        min: [0, 'Price must be a positive number']
-      },
-      flavor: {
-        type: String,
-        enum: {
-          values: ['Sweet', 'Sour', 'Salty', 'Spicy', 'Bitter', 'Umami'],
-          message: 'Flavor is not valid'
-        }
-      },
-      dietrestriction: {
-        type: String,
-        enum: {
-          values: ['Gluten Free', 'Nut Free', 'Dairy Free', 'Egg Free', 'Sugar Free', 'Vegan'],
-          message: 'Diet restriction is not valid'
-        }
-      },
-      ingredients: {
-        type: [String]
-      }
-    });
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    minlength: [3, "Name must be at least 3 characters long"],
+    maxlength: [50, "Name must be at most 50 characters long"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Price is required"],
+    min: [0, "Price must be a positive number"],
+  },
+  flavor: {
+    type: String,
+  },
+  dietrestriction: {
+    type: String,
+    enum: {
+      values: [
+        "Gluten Free",
+        "Nut Free",
+        "Dairy Free",
+        "Egg Free",
+        "Sugar Free",
+        "Vegan",
+      ],
+      message: "Diet restriction is not valid",
+    },
+  },
+  ingredients: {
+    type: [String],
+  },
+});
 
-export default mongoose.model('Product', ProductSchema);
+export default mongoose.model("Product", ProductSchema);
