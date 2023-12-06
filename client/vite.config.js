9fdf3ac9b8a4b87814daea52581ae8fa4be4ce3a
Fatima-04 <146798356+Fatima-04@users.noreply.github.com>
2023-12-06 02:42:37 -0500

    host attribute added

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,24 @@
-import path from 'path';
-import dotenv from 'dotenv';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import process from 'process';
+import path from "path";
+import dotenv from "dotenv";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import process from "process";
 
 dotenv.config();
 
 const { PORT = 3000 } = process.env;
 
 export default defineConfig({
-  base: './',
+  base: "./",
   plugins: [react()],
   server: {
+    hose: true,
     proxy: {
-      '/api': {
+      "/api": {
         target: `http://localhost:${PORT}`,
         changeOrigin: true,
       },
-      '/auth': {
+      "/auth": {
         target: `http://localhost:${PORT}`,
         changeOrigin: true,
       },
@@ -28,13 +29,13 @@ export default defineConfig({
     // rollupOptions: {
     //   input: './src/main.jsx',
     // },
-    outDir: '../dist/app',
+    outDir: "../dist/app",
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // Adjust the alias as per your project structure
-      '@client': path.resolve(__dirname, 'src/client'),
-      '@user': path.resolve(__dirname, 'user'), // Corrected the alias for user
+      "@": path.resolve(__dirname, "src"), // Adjust the alias as per your project structure
+      "@client": path.resolve(__dirname, "src/client"),
+      "@user": path.resolve(__dirname, "user"), // Corrected the alias for user
     },
   },
 });
